97070743854eb6df8178e2ebc9781b19ded2129a
Tim Wagner <t.wagner@techdivision.com>
2015-02-20 13:42:58 +0100

    Add ClassLoaderInterface, ClassLoaderConfigurationInterface + ManagerConfiguration interface

---
+++ src/AppserverIo/Psr/Application/ManagerConfigurationInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * AppserverIo\Psr\Application\ManagerConfigurationInterface
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Tim Wagner <tw@appserver.io>
+ * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/appserver-io-psr/application
+ * @link      http://www.appserver.io
+ */
+
+namespace AppserverIo\Psr\Application;
+
+/**
+ * Interface for the manager configuration node.
+ *
+ * @author    Tim Wagner <tw@appserver.io>
+ * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/appserver-io-psr/application
+ * @link      http://www.appserver.io
+ */
+interface ManagerConfigurationInterface
+{
+
+    /**
+     * Returns the application name.
+     *
+     * @return string The unique application name
+     */
+    public function getName();
+}
